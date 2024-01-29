1e0c81b67169412c4a1a9fc18731540f2e698a1a
chenzifeng <chen1677@purdue.edu>
2024-01-29 16:57:25 +0800

    fix: any type

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000'
 
-export const addUser = (data) => axios.post('/user', data).then((res) => res.data)
+export const addUser = (data: any) => axios.post('/user', data).then((res) => res.data)
 
 export const getList = (data) => axios.get('/user', { params: data }).then((res) => res.data)
 
